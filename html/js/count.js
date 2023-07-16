356f2bade968dcd9e369fe94e2059c2867df07e5
X <138562024+xbcen@users.noreply.github.com>
2023-07-16 21:00:30 +0800

    count.js

--- html/js/count.js
+++ html/js/count.js
@@ -1,6 +1,6 @@
         var liElements = document.getElementsByTagName('li');
         var liCount = liElements.length;
-        var additionalNumber = 6;
+        var additionalNumber = 5;
         var result = liCount + additionalNumber;
         document.getElementById('result').innerHTML = "收录中...（已收录" + result + "个字体）";
   
