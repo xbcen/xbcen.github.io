eba5a807ca1408b15d1942361591532593bef40c
X <138562024+xbcen@users.noreply.github.com>
2023-07-16 19:06:57 +0800

    search.js

--- html/js/search.js
+++ html/js/search.js
@@ -1,16 +1,28 @@
 function myFunction() {
-  var input, filter, ul, li, a, i;
+  var input, filter, ul, li, a, i, h2;
   input = document.getElementById('mySearch');
   filter = input.value.toUpperCase();
-  ul = document.getElementById('myMenu');
-  li = ul.getElementsByTagName('li');
+  ul = document.getElementById('myMenu').getElementsByTagName('ul');
 
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName('a')[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+  for (i = 0; i < ul.length; i++) {
+    li = ul[i].getElementsByTagName('li');
+    h2 = ul[i].previousElementSibling;
+    var visibleLiCount = 0;
+
+    for (var j = 0; j < li.length; j++) {
+      a = li[j].getElementsByTagName('a')[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[j].style.display = "";
+        visibleLiCount++;
+      } else {
+        li[j].style.display = "none";
+      }
+    }
+
+    if (visibleLiCount > 0) {
+      h2.style.display = "";
     } else {
-      li[i].style.display = "none";
+      h2.style.display = "none";
     }
   }
 }
