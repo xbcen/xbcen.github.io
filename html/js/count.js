2626d2fcd3afcb10561c7e30548d72db006d4c7d
X <138562024+xbcen@users.noreply.github.com>
2023-07-16 17:34:32 +0800

    Add files via upload

---
+++ html/js/count.js
@@ -0,0 +1,6 @@
+        var liElements = document.getElementsByTagName('li');
+        var liCount = liElements.length;
+        var additionalNumber = 6;
+        var result = liCount + additionalNumber;
+        document.getElementById('result').innerHTML = "收录中...（已收录" + result + "个字体）";
+  
