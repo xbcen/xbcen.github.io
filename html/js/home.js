f1953c19b99a43bc7c1080677b3fd486f5c867a5
X <138562024+xbcen@users.noreply.github.com>
2023-07-19 21:48:33 +0800

    home.js

--- html/js/home.js
+++ html/js/home.js
@@ -1,7 +1,10 @@
+let data;
 async function getData() {
     const response = await fetch("https://v1.hitokoto.cn/?c=i&encode=text");
-    const data = await response.text();
+    data = await response.text();
     document.getElementById("cen").innerHTML = data;
 }
-
+document.getElementById("cen").addEventListener('click', function() {
+    window.open('https://www.baidu.com/s?ie=UTF-8&wd=' + data);
+});
 getData();
