c216541c12bb648ddde55b9ceb846a303f9142e3
X <138562024+xbcen@users.noreply.github.com>
2023-07-20 13:40:44 +0800

    search.js

--- html/js/search.js
+++ html/js/search.js
@@ -1,28 +1,34 @@
-function myFunction() {
-  var input, filter, ul, li, a, i, h2;
-  input = document.getElementById('mySearch');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById('myMenu').getElementsByTagName('ul');
+document.getElementById('mySearch').addEventListener('keyup', function () {
+    var input, filter, ul, li, a, i, h2;
+    input = document.getElementById('mySearch');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('myMenu').getElementsByTagName('ul');
 
-  for (i = 0; i < ul.length; i++) {
-    li = ul[i].getElementsByTagName('li');
-    h2 = ul[i].previousElementSibling;
-    var visibleLiCount = 0;
+    for (i = 0; i < ul.length; i++) {
+        li = ul[i].getElementsByTagName('li');
+        h2 = ul[i].previousElementSibling;
+        var hasVisibleChild = false;
 
-    for (var j = 0; j < li.length; j++) {
-      a = li[j].getElementsByTagName('a')[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[j].style.display = "";
-        visibleLiCount++;
-      } else {
-        li[j].style.display = "none";
-      }
+        for (var j = 0; j < li.length; j++) {
+            a = li[j].getElementsByTagName('a')[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[j].style.display = "";
+                hasVisibleChild = true;
+            } else {
+                li[j].style.display = "none";
+            }
+        }
+
+        if (hasVisibleChild) {
+            if (h2) { 
+                h2.style.display = ""; 
+            }
+            ul[i].style.display = ""; 
+        } else {
+            if (h2) { 
+                h2.style.display = "none";
+            }
+            ul[i].style.display = "none"; 
+        }
     }
-
-    if (visibleLiCount > 0) {
-      h2.style.display = "";
-    } else {
-      h2.style.display = "none";
-    }
-  }
-}
+});
