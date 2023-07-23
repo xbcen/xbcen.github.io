e0c9d658ec40ac0a8a393d02968fa5e02e25c798
X <138562024+xbcen@users.noreply.github.com>
2023-07-23 17:41:04 +0800

    Add files via upload

---
+++ html/js/download.js
@@ -0,0 +1,12 @@
+function generateNewUrl() {
+  var user_url = document.getElementById('userUrl').value;
+  var parsed_url = new URL(user_url);
+  var content_id = parsed_url.searchParams.get('contentId') || 'abc';
+
+  var old_url = "https://r2-ndr.ykt.cbern.com.cn/edu_product/esp/assets_document/abc.pkg/pdf.pdf";
+  var new_url = old_url.replace('abc',content_id);
+  
+  var linkElement = document.getElementById('newUrlLink');
+  linkElement.href = new_url;
+  linkElement.textContent = "点击下载";
+}
