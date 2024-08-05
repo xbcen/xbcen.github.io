695a25df7ef48fbfee89eafa8f5d379037ede7cf
xbcen <3308147083@qq.com>
2024-08-05 14:37:28 +0800

    85141

---
+++ html/js/time.js
@@ -0,0 +1,14 @@
+let clock = document.querySelector('#clock');
+let two = (num) => {
+    if (num >= 10) {
+        return num.toString();
+    } else {
+        return `0${num}`;
+    }
+}
+let udtime = () => {
+    let now = new Date();
+    let time = two(now.getHours()) + ":" + two(now.getMinutes()) + ":" + two(now.getSeconds());
+    clock.innerText = time;
+}
+setInterval(udtime, 500);
