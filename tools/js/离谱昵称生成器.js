89d2feb96325e0fc468c1e0d675c17dc1938c26c
xbcen <3308147083@qq.com>
2025-01-16 21:26:21 +0800

    name

--- tools/js/离谱昵称生成器.js
+++ tools/js/离谱昵称生成器.js
@@ -1,48 +1,54 @@
-var adjectives = [],
-  verbs = [],
-  nouns = [];
+let adjectives = [];
+let verbs = [];
+let nouns = [];
 
 $.getJSON("other/words-agd.json", function (data) {
   adjectives = data.adjectives;
   verbs = data.verbs;
   nouns = data.nouns;
 
-    function getRandomItem(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+  // 从数组中随机获取一个元素
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
   }
 
-    function generatePhrase() {
-    var adjective = getRandomItem(adjectives);
-    var verb = getRandomItem(verbs);
-    var noun = getRandomItem(nouns);
-    return `${adjective}的${noun}在${verb}`;
+  // 生成随机短语
+  function generateRandomPhrase() {
+   //const adjective = getRandomElement(adjectives);
+    const verb = getRandomElement(verbs);
+    const noun = getRandomElement(nouns);
+    return `${noun}在${verb}`;
   }
 
-    $("#generateBtn").on("click", function () {
-    var phrase = generatePhrase();
+  // 点击生成按钮事件处理
+  $("#generateBtn").on("click", function () {
+    const phrase = generateRandomPhrase();
     $("#phrase").text(phrase);
   });
 });
 
+// 点击复制按钮事件处理
 document.getElementById("copyBtn").addEventListener("click", function () {
-  var phraseText = document.getElementById("phrase").textContent;
-  copyToClipboard(phraseText);
+  const phraseText = document.getElementById("phrase").textContent;
+  copyTextToClipboard(phraseText);
 });
 
-function copyToClipboard(text) {
-  var dummy = document.createElement("textarea");
-  document.body.appendChild(dummy);
-  dummy.value = text;
-  dummy.select();
+// 将文本复制到剪贴板
+function copyTextToClipboard(text) {
+  const tempTextArea = document.createElement("textarea");
+  document.body.appendChild(tempTextArea);
+  tempTextArea.value = text;
+  tempTextArea.select();
   document.execCommand("copy");
-  document.body.removeChild(dummy);
-  showCopiedMessage();
+  document.body.removeChild(tempTextArea);
+  displayCopiedMessage();
 }
 
-function showCopiedMessage() {
-  var message = document.getElementById("copied");
-  message.style.opacity = 1;
+// 显示复制成功的消息
+function displayCopiedMessage() {
+  const messageElement = document.getElementById("copied");
+  messageElement.style.opacity = 1;
   setTimeout(function () {
-    message.style.opacity = 0;
+    messageElement.style.opacity = 0;
   }, 2500);
 }
